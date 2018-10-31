bda296c775c5d765e2a8419f29d41b7dc673b505
austinellingwood <austinellingwood@gmail.com>
2018-10-31 16:32:20 -0400

    Update entrypoint.sh

--- mysqlDocker/entrypoint.sh
+++ mysqlDocker/entrypoint.sh
@@ -135,7 +135,7 @@ EOF
 			echo "GRANT ALL ON "$MYSQL_DATABASE".* TO 'root'@'%';" | "${mysql[@]}"
 			echo "show databases;" | "${mysql[@]}"
 			echo "SELECT DATABASE();" | "${mysql[@]}"
-			echo "USE stage;" | "${mysql[@]}"
+			echo "USE "$MYSQL_DATABASE";" | "${mysql[@]}"
 			echo "SELECT DATABASE();" | "${mysql[@]}"
 			echo "source "$MYSQL_DATABASE_PATH";" | "${mysql[@]}" 
 		fi
