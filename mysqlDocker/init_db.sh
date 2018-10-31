7dda917f0cba7553686e6e88ff4365347763d7e0
austinellingwood <austinellingwood@gmail.com>
2018-10-31 13:37:48 -0400

    Update init_db.sh

--- mysqlDocker/init_db.sh
+++ mysqlDocker/init_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-/usr/bin/mysqld_safe --skip-grant-tables &
-sleep 5
-mysql -u root -e "CREATE DATABASE stage"
-mysql -u root mydb < /tmp/stage.sql
+VAR=$(docker logs mysql-dock2 2>&1 | grep GENERATED)
+docker exec -it mysql-dock$1 mysql -u root -p$VAR -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('password')"
+#sleep 5
+mysql -u root -ppassword -e "CREATE DATABASE stage"
+mysql -u root -ppassword stage < /tmp/stage.sql
 
 
 ######init_db.sh########
